9fc94b2b9e967e9d9ec1465411b6548fb282c1e6
Taha Saleh <taha.a.saleh@gmail.com>
2019-11-10 19:05:37 -0600

    add unit test

---
+++ test/pingApi.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+using pingApi.contollers;
+
+namespace pingApi.Test
+{
+    public class UnitTest1
+    {
+        PingController myContorller = new PingController();
+
+        [Fact]
+        public void GetReturnsPong()
+        {
+            var returnValue = myContorller.Ping();
+            Assert.Equal("pong",returnValue.ToString());
+        }
+    }
+}
