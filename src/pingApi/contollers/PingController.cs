c90404d01c644398140ef710d795af748bcad035
Taha Saleh <taha.a.saleh@gmail.com>
2019-11-10 19:05:25 -0600

    create ping endpoint

---
+++ src/pingApi/contollers/PingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pingApi.contollers
+{
+    [Route("api/ping")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public string Ping() {
+            return "pong";
+
+        }
+    }
+}
